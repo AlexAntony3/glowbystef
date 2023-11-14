6c7a161beeb7f44424ec6dfe4e8e716c16db92ad
AlexAntony3 <alex.antony.jy@gmail.com>
2023-11-14 17:03:58 +0000

    feat: add log in and out views and set up permissions to serializer for profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 'bio',
-            'contact_number', 'profile_picture'
+            'contact_number', 'profile_picture', 'is_owner',
         ]
