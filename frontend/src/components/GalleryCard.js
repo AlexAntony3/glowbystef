15f7a9f46bf74091cad5e58df7a7dc9630fb7dc2
AlexAntony3 <alex.antony.jy@gmail.com>
2023-11-26 22:13:38 +0000

    fix: update image sizes for gallery immages

--- frontend/src/components/GalleryCard.js
+++ frontend/src/components/GalleryCard.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-import btnStyles from "../styles/Button.module.css";
 import styles from "../styles/Gallery.module.css";
-const GalleryCard = ({ title, description, image }) => {
+const GalleryCard = ({ title, description, image, likes }) => {
   return (
     <Card className={styles.GalleryContentDisplay}>
       <Card.Img variant="top" src={image} />
       <Card.ImgOverlay>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Button className="btn-warning fa-solid fa-heart" type="button" />
+        <Button className="LikeButtonClicked btn-warning fa-solid fa-heart" type="button" />
       </Card.ImgOverlay>
     </Card>
   );
