cd3671312915c76babd2b6870b3c3c47218cbaa9
AlexAntony3 <alex.antony.jy@gmail.com>
2023-11-30 21:45:18 +0000

    FilterBar component update to implement search functionality

--- frontend/src/components/FilterBar.js
+++ frontend/src/components/FilterBar.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  Button,
+  ButtonGroup,
+  Col,
+  Form,
+  Row,
+  ToggleButton,
+} from "react-bootstrap";
 import styles from "../styles/FilterBar.module.css";
 
-const FilterBar = () => {
+const FilterBar = ({ setSearchTerm }) => {
+  const [term, setTerm] = useState("");
+  const radioOptions = [
+    { name: "All", value: "1" },
+    { name: "Liked", value: "2" },
+    { name: "My Uploads", value: "3" },
+  ];
+  const [radioValue, setRadioValue] = useState("1");
+
   return (
     <Row className={styles.FilterBar}>
       <Col sm={4}>
@@ -12,23 +27,34 @@ const FilterBar = () => {
             placeholder="E.g. funky nails"
             className="me-2 rounded-pill"
             aria-label="Search"
+            onChange={(event) => setTerm(event.target.value)}
           />
-          <Button className="rounded-pill" variant="outline-secondary">
+          <Button
+            className="rounded-pill"
+            variant="outline-secondary"
+            onClick={() => term && setSearchTerm(term)}
+          >
             Search
           </Button>
         </Form>
       </Col>
 
       <Col>
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button>Search</Button>
-        </Form>
+        <ButtonGroup toggle>
+          {radioOptions.map((option, idx) => (
+            <ToggleButton
+              key={idx}
+              type="radio"
+              variant="secondary"
+              name="radio"
+              value={option.value}
+              checked={radioValue === option.value}
+              onChange={(e) => setRadioValue(e.currentTarget.value)}
+            >
+              {option.name}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
       </Col>
       <Col>
         <Form className="d-flex">
--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -1,33 +1,54 @@
 import React, { useEffect, useState } from "react";
-import appStyles from "../App.module.css";
 import { axiosRes } from "../api/axiosDefaults";
-import GalleryCard from "../components/GalleryCard";
+import appStyles from "../App.module.css";
+import styles from "../styles/Gallery.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSliders } from "@fortawesome/free-solid-svg-icons";
-import styles from "../styles/Gallery.module.css";
-import FilterBar from "../components/FilterBar";
 import { CardColumns, Container } from "react-bootstrap";
+import GalleryCard from "../components/GalleryCard";
+import FilterBar from "../components/FilterBar";
 
 const Gallery = () => {
   const [galleryImages, setGalleryImages] = useState([]);
   const [filterable, setFilterable] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
 
   useEffect(() => {
     getGalleryImages();
   }, []);
 
+  useEffect(() => {
+    filterResults();
+    setGalleryImages(filteredResults);
+  }, [searchTerm]);
+
+  const filterResults = () => {
+    const list = new Array();
+    searchTerm &&
+      results.map((result) => {
+        if (result.title.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0) {
+          list.push(result);
+        }
+      });
+    list.length !== 0 ? setFilteredResults(() => list) : getGalleryImages();
+  };
+
   const getGalleryImages = async () =>
     await axiosRes
       .get("/galleries/")
       .then((response) => {
+        setResults(response.data.results);
         setGalleryImages(response.data.results);
       })
       .catch((err) => {
+        setResults([]);
         setGalleryImages([]);
       });
 
   return (
-    <Container className={`${appStyles.Content} justify-content-center p-4 `}>
+    <Container className={`${appStyles.Content}`}>
       <h1 className={appStyles.Header}>
         Gallery
         <span
@@ -38,7 +59,9 @@ const Gallery = () => {
         </span>
       </h1>
 
-      {filterable && <FilterBar className={styles.FilterBar} />}
+      {filterable && (
+        <FilterBar className={styles.FilterBar} setSearchTerm={setSearchTerm} />
+      )}
 
       <CardColumns>
         {galleryImages.map((image) => {
